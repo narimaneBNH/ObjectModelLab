85246ab7c5112076beccfcf6f2670f1ee1b7d891
Narimane <n.benhellal@gmail.com>
2016-11-19 21:23:26 +0100

    TP2 ObjectModelLab Narimane BENHELLAL

--- src/model/index.js
+++ src/model/index.js
@@ -1,2 +1,195 @@
+/* M2 MATIS SIRES */
+/* Ce TP a été réalisé par: Narimane BENHELLAL*/
 
 /* TODO : Créer le modèle objet ici */ 
+
+export class Data {
+	constructor() {
+	}
+}
+
+export class SensorType{
+	constructor(){
+		this.types = ['Temperature', 'Humidity', 'Light', 'Switch', 'Door', 'FAN_SPEED'];
+	}
+	get type(){
+		this.types = type;
+	}
+	set type(type){
+		this.types = type;
+	}
+	if (typeof types === 'string' || types instanceof String){
+		if (types = "Temperature"){
+			return new Temperature(data);
+		}
+		if (types = "Humidity"){
+			return new Humidity(data);
+		}
+		if (types = "Light"){
+			return new Light(data);
+		}
+		if (types = "Switch"){
+			return new Door(data);
+		}
+		if (types = "FAN_SPEED"){
+			return new FAN_SPEED(data);
+		}
+		if (types = "Door"){
+			return new Door(data);
+		}
+	}
+}
+
+export class Sensor {
+	constructor(id, name, data){
+		this.identifiant = id;
+ 		this.nom = name;
+ 		this.donnee = new Data(data);
+		
+		if(!Number.isInteger(data.id){
+			throw "ID doit être un nombre entier";
+		}
+		this.types = new SensorType();
+	}
+	get id(){
+		return this.identifiant || 0;
+	}
+	set id(iD){
+		this.identifiant = iD;
+	}
+	
+	get name(){
+     		return this.nom || 0; 
+   	}
+   	set name(name){
+    		this.nom = name;
+   	}		
+	
+	get data() {
+        	return this.donnee;
+    	}
+    	set data(data) {
+        	this.donnee = data;
+    	}
+	
+	get type(){
+		return this.typeSensor || 0;
+	}
+	set type(t){
+		this.typeSensor = t;
+	}
+}
+
+
+export class TimeSeries extends Data{
+	constructor(value, label){
+		super();
+		this.values = value;
+		this.labels = label;
+	}
+	get value(){
+		return this.values;
+	}
+	set value(val){
+		this.values = val;
+    	}
+	
+    	get label() {
+        	return this.labels;
+    	}
+	set label(lab) {
+        	this.labels = lab;
+   	 }
+	
+    	getCountVal(){
+		return this.values.length;
+	}
+	
+	getLabel(){
+		var l = new Date("0"); //définir la dernière date
+		for(let i in this.values){
+			var date = new Date(this.labels[i]);
+			if(date.getTime() > l.getTime()){
+				l = date;
+			}
+		}
+		return l;
+	}
+	
+	getValue(){
+		var l = new Date("0"); //définir la dernière date 
+		var v = 0; //définir la dernière valeur
+		for(let i in this.values){
+			var date = new Date(this.labels[i]);
+			if(date.getTime() > l.getTime()){
+				l = date;
+				v = this.values[i];
+			}
+		}
+		return v;
+	}
+	
+	getMoy(){
+		var som = 0;
+		for(let i in this.values){
+			som += this.values[i];
+		}
+		let resultat = som / this.getCountVal();
+		resultat = resultat.toFixed(2);
+
+        return resultat;
+	}
+}
+
+export class Datum extends Data{
+	constructor(value){
+		super();
+		this.valeur = value;
+	}
+	get value(){
+		return this.valeur || 0;
+	}
+	set value(value){
+		this.val = value;
+	}
+	dataOpen () {
+		return this.valeur == 1;
+	}
+}
+
+
+export class Temperature extends Sensor{
+	constructor(id, name, types){
+		super(id, name, types);
+		this.donnee = new TimeSeries();
+	}
+}
+
+export class Humidity extends Sensor{
+	constructor(id, name, types){
+		super(id, name, types);
+	}
+}
+
+export class Light extends Sensor{
+	constructor(id, name, types){
+		super(id, name, types);
+	}
+}
+
+
+export class Door extends Sensor{
+	constructor(id, name, types){
+		super(id, name, types);
+		this.donnee = new Datum();
+	}
+}
+
+export class FAN_SPEED extends Sensor{
+	constructor(id, name, types){
+		super(id, name, types);
+		this.donnee = new TimeSeries();
+	}
+} 
+
+
--- test/model/index.js
+++ test/model/index.js
@@ -2,9 +2,89 @@ import expect from 'expect';
 
 // import { ??? } from '../../src/model';
 
+import {Data} from '../../src/model';
+import {Sensor} from '../../src/model';
+import {TimeSeries} from '../../src/model';
+import {Datum} from '../../src/model';
+import {Temperature} from '../../src/model';
+import {FAN_SPEED} from '../../src/model';
+import {Door} from '../../src/model';
+
 import  { data }  from './sensors_data'
 console.log(data.length);
 
+import resultat from '../../src/model';
+let t = resultat();
+
 describe('Sensor model tests', () => {
-  /* TODO: Ecrire ici la suite de tests pourle modèle objet.*/
+  /* TODO: Ecrire ici la suite de tests pour le modèle objet.*/
+
+  describe('TimeSeries', () => {
+        it('Test du premier objet', () => {
+            expect(typeof(t[0]) === 'object').toBe(true);
+        });
+
+        it('L\'objet = Temperature',() => {
+            expect(t[0].type).toEqual('TEMPERATURE');
+        });
+
+        t[0].name = 'Température de Bureau';
+
+        it('Test sur l\'objet Température', () => {
+            expect(t[0].name).toEqual('Température de Bureau');
+        });
+
+        it('Température moyenne', () => {
+            expect(t[0].data.getMoy() == 23.11).toBe(true); // (23+23+22+21+23+23+23+25+25)/9 = 23.11
+        });
+		
+	it('Mesure de la température (finale)', () => {
+            expect(t[0].data.getLabel()).toEqual('2016-10-19T14:00:00.000Z');
+        });
+		
+	});
+	
+	describe('Test du deuxième objet', () => {
+        it('L\'entité 2 est un objet', () => {
+            expect(typeof(t[1]) === 'object').toBe(true);
+        });
+
+        it('L\'objet = Door', () => {
+            expect(t[1].type).toEqual('DOOR');
+        });
+
+        t[1].name = 'Porte du Garage';
+
+        it('Test sur l\'objet Door', () => {
+            expect(t[1].name).toEqual('Porte du Garage');
+        });
+
+        it('porte est ouverte / fermée', () => {
+            expect(t[1].data.isOpen()).toBe(false);
+        });
+    });
+	
+	describe('Test du troisième objet', () => {
+        it('L\'entité 3 est un objet', () => {
+            expect(typeof(t[2]) === 'object').toBe(true);
+        });
+
+        it('L\'objet = FAN_SPEED', () => {
+            expect(t[2].type).toEqual('FAN_SPEED');
+        });
+
+        tab[2].name = 'Ventilateur Ordinateur Bureau';
+
+        it('Test sur l\'objet FAN_SPEED', () => {
+            expect(t[2].name).toEqual('Ventilateur Ordinateur Bureau');
+        });
+
+        it('Test de ventilation moyenne', () => {
+            expect(t[2].data.getMoy() == 1774.5).toBe(true); // (1073+1800+2299+2176+1899+1400) = 1774.5
+        });
+		
+		it('Mesure de la ventilation (finale)', () => {
+            expect(t[2].data.getLabel()).toEqual('2016-10-19T10:15:00.000Z');
+        });
+    });
 });
